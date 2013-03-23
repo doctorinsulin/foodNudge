62aa3e0806b75fc12c4e3ed90d09b490a1ca2e0c
Ben Jeffrey <mail@benjeffrey.net>
2013-03-23 13:55:40 +0000

    Added Food form and wtforms requirement

---
+++ forms.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from wtforms import Form, SelectField
+
+
+class Foods(Form):
+    """
+    WTForms class, allowing the selection of up to 3 foods.
+    The foods available must be defined in the handler, as in
+    <http://wtforms.simplecodes.com/docs/1.0.3/fields.html#wtforms.fields.SelectField>
+    """
+    food1 = SelectField(u'food1')
+    food2 = SelectField(u'food2')
+    food3 = SelectField(u'food3')
